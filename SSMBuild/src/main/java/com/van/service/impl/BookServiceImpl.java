ff46a27efc6ea8b417d55b5f51b0d6964288506d
danielvan <2458238922@qq.com>
2021-03-28 12:44:41 +0800

    ssm

--- SSMBuild/src/main/java/com/van/service/impl/BookServiceImpl.java
+++ SSMBuild/src/main/java/com/van/service/impl/BookServiceImpl.java
@@ -1,28 +1,36 @@
 package com.van.service.impl;
 
+import com.van.dao.BookMapper;
 import com.van.pojo.Books;
 import com.van.service.BookService;
 
 import java.util.List;
 
 public class BookServiceImpl implements BookService {
+
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
     public int addBook(Books books) {
-        return 0;
+        return bookMapper.addBook(books);
     }
 
     public int deleteBook(int id) {
-        return 0;
+        return bookMapper.deleteBook(id);
     }
 
     public int updateBook(Books books) {
-        return 0;
+        return bookMapper.updateBook(books);
     }
 
     public Books queryBook(int id) {
-        return null;
+        return bookMapper.queryBook(id);
     }
 
     public List<Books> queryBooks() {
-        return null;
+        return bookMapper.queryBooks();
     }
 }
