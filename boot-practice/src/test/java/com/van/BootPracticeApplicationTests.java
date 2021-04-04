c893f19bad057bdbbbcdcfa313abf0dc02f4bda8
danielvan <2458238922@qq.com>
2021-04-04 16:30:16 +0800

    yaml

---
+++ boot-practice/src/test/java/com/van/BootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.van;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
